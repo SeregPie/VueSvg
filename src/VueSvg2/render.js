cec180ebb76395c9e738d87e5297808ab0f1faa2
Sergej Sintschilin <seregpie@gmail.com>
2018-10-24 21:37:52 +0200

    VueSvg without IE11

---
+++ src/VueSvg2/render.js
@@ -0,0 +1,12 @@
+export default function(createElement) {
+	let {
+		svgAttributes,
+		svgContent,
+	} = this;
+	return createElement('svg', {
+		attrs: svgAttributes,
+		domProps: {
+			innerHTML: svgContent,
+		},
+	});
+}
